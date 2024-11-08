bfad4793a00de207cbd6cc9d845f310b5439e9cf
Michael-Eng-ss <ms4143403@gmail.com>
2024-11-08 00:46:17 -0300

    Mudança da Classe UsuarioDAO para um método,atribuição verificação hasNextInt()

--- src/main/java/manage_service/UsuarioService.java
+++ src/main/java/manage_service/UsuarioService.java
@@ -9,43 +9,43 @@ public class UsuarioService {
 
     private static final UsuarioDAO usuarioDAO = new UsuarioDAO();
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public void menuUsuario(Scanner scanner) {
         int opcao;
 
         do {
             System.out.println("\n--- Menu ---");
             System.out.println("1. Cadastrar Usuario");
             System.out.println("2. Consultar Usuario");
-            System.out.println("3. Sair");
+            System.out.println("3. Voltar ao Menu Principal");
             System.out.print("Escolha uma opção: ");
-            opcao = scanner.nextInt();
-            scanner.nextLine(); // Consumir a quebra de linha
 
-            switch (opcao) {
-                case 1:
-                    cadastrarUsuario(scanner);
-                    break;
-                case 2:
-                    consultarUsuario(scanner);
-                    break;
-                case 3:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção inválida. Tente novamente.");
+
+            if (scanner.hasNextInt()) {
+                opcao = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (opcao) {
+                    case 1:
+                        cadastrarUsuario(scanner);
+                        break;
+                    case 2:
+                        consultarUsuario(scanner);
+                        break;
+                    case 3:
+                        System.out.println("Voltando ao Menu Principal");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Tente novamente.");
+                }
+            } else {
+                System.out.println("Entrada inválida. Por favor, insira um número.");
+                scanner.nextLine();
+                opcao = -1;
             }
         } while (opcao != 3);
-
-        scanner.close();
     }
 
-    private static void inativarProduto(Scanner scanner) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'inativarProduto'");
-    }
-
-    private static void cadastrarUsuario(Scanner scanner) {
+    private void cadastrarUsuario(Scanner scanner) {
         System.out.print("Digite o seu nome: ");
         String nome = scanner.nextLine();
         System.out.print("Digite o seu email: ");
@@ -59,17 +59,25 @@ public class UsuarioService {
 
         Usuario usuario = new Usuario(nome, email, endereco, telefone, profissao);
         usuarioDAO.create(usuario);
+        System.out.println("Usuário cadastrado com sucesso.");
     }
 
-    private static void consultarUsuario(Scanner scanner) {
+    private void consultarUsuario(Scanner scanner) {
         System.out.print("Digite o ID do usuario que deseja consultar: ");
-        int id = scanner.nextInt();
 
-        Usuario usuario = usuarioDAO.get(id);
-        if (usuario != null) {
-            System.out.println("Usuario encontrado: " + usuario);
+        if (scanner.hasNextInt()) {
+            int id = scanner.nextInt();
+            scanner.nextLine();
+
+            Usuario usuario = usuarioDAO.get(id);
+            if (usuario != null) {
+                System.out.println("Usuario encontrado: " + usuario);
+            } else {
+                System.out.println("Usuario com ID " + id + " não encontrado.");
+            }
         } else {
-            System.out.println("Usuario com ID " + id + " não encontrado.");
+            System.out.println("Entrada inválida. O ID deve ser um número.");
+            scanner.nextLine();
         }
     }
 }
