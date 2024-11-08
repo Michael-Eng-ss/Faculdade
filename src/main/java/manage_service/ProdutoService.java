f4cd2de10d0924de81a78bfb7cf5e5e3cc8cd620
Michael-Eng-ss <ms4143403@gmail.com>
2024-11-08 00:44:09 -0300

    Mudança da Classe ProdutoService para um método,atribuição verificação hasNextInt()

--- src/main/java/manage_service/ProdutoService.java
+++ src/main/java/manage_service/ProdutoService.java
@@ -6,124 +6,143 @@ import manage_model.Produto;
 import java.util.Scanner;
 
 public class ProdutoService {
-    private static final ProdutoDAO produtoDAO = new ProdutoDAO();
-    static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    private static final ProdutoDAO produtoDAO = new ProdutoDAO();
+
+    public void menuProduto(Scanner scanner) {
         int opcao;
 
         do {
-            System.out.println("\n--- Menu ---");
+            System.out.println("\n--- Menu Produto ---");
             System.out.println("1. Cadastrar Produto");
             System.out.println("2. Consultar Produto");
             System.out.println("3. Desativar Produto");
             System.out.println("4. Atualizar Produto");
-            System.out.println("5. Sair");
+            System.out.println("5. Voltar ao Menu Principal");
             System.out.print("Escolha uma opção: ");
-            opcao = scanner.nextInt();
-            scanner.nextLine(); // Consumir a quebra de linha
 
-            switch (opcao) {
-                case 1:
-                    cadastrarProduto();
-                    break;
-                case 2:
-                    consultarProduto();
-                    break;
-                case 3:
-                    desativarProdutoService();
-                    break;
-                case 4:
-                    atualizarProdutoService();
-                    break;
-                case 5:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção inválida. Tente novamente.");
+            if (scanner.hasNextInt()) {
+                opcao = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (opcao) {
+                    case 1:
+                        cadastrarProduto(scanner);
+                        break;
+                    case 2:
+                        consultarProduto(scanner);
+                        break;
+                    case 3:
+                        desativarProdutoService(scanner);
+                        break;
+                    case 4:
+                        atualizarProdutoService(scanner);
+                        break;
+                    case 5:
+                        System.out.println("Voltando ao Menu Principal...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Tente novamente.");
+                }
+            } else {
+                System.out.println("Entrada inválida. Por favor, insira um número.");
+                scanner.nextLine();
+                opcao = -1;
             }
         } while (opcao != 5);
-
-        scanner.close();
     }
 
-    private static void inativarProduto( ) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'inativarProduto'");
-    }
-
-    private static void cadastrarProduto() {
+    private void cadastrarProduto(Scanner scanner) {
         System.out.print("Digite o nome do produto: ");
         String nome = scanner.nextLine();
+
+        System.out.println("Digite sua marca do produto: ");
+        String marca = scanner.nextLine();
 
         System.out.print("Digite o preço do produto: ");
         double preco = 0;
 
         try {
-            preco = Double.parseDouble(scanner.nextLine()); // Lê a linha como string e converte para double
+            preco = Double.parseDouble(scanner.nextLine());
         } catch (NumberFormatException e) {
             System.out.println("Preço inválido! Tente novamente.");
             return;
         }
 
-        Produto produto = new Produto(nome, preco);
+        Produto produto = new Produto(nome, preco,marca);
         produtoDAO.create(produto);
-
         System.out.println("Produto cadastrado com sucesso!");
     }
 
-
-    private static void consultarProduto( ) {
+    private void consultarProduto(Scanner scanner) {
         System.out.print("Digite o ID do produto que deseja consultar: ");
-        int id = scanner.nextInt();
-        scanner.nextLine(); // Consumir a quebra de linha
 
-        Produto produto = produtoDAO.get(id);
-        if (produto != null) {
-            System.out.println("Produto encontrado: " + produto);
+        if (scanner.hasNextInt()) {
+            int id = scanner.nextInt();
+            scanner.nextLine();
+
+            Produto produto = produtoDAO.get(id);
+            if (produto != null) {
+                System.out.println("Produto encontrado: " + produto);
+            } else {
+                System.out.println("Produto com ID " + id + " não encontrado.");
+            }
         } else {
-            System.out.println("Produto com ID " + id + " não encontrado.");
+            System.out.println("Entrada inválida. O ID deve ser um número.");
+            scanner.nextLine();
         }
     }
 
-    private static void desativarProdutoService() {
+    private void desativarProdutoService(Scanner scanner) {
         System.out.print("Digite o ID do produto que deseja desativar: ");
-        int id = scanner.nextInt();
-        scanner.nextLine(); // Consome a nova linha deixada após o nextInt()
 
-        // Desativa o produto pelo ID
-        produtoDAO.delete(id);
+        if (scanner.hasNextInt()) {
+            int id = scanner.nextInt();
+            scanner.nextLine();
 
-    }
-
-    private static void atualizarProdutoService() {
-        System.out.print("Digite o ID do produto que deseja atualizar: ");
-        int id = scanner.nextInt();
-        scanner.nextLine(); // Consumir a quebra de linha
-
-        // Consulta o produto pelo ID para verificar se ele existe
-        Produto produtoExistente = produtoDAO.get(id);
-        if (produtoExistente == null) {
-            System.out.println("Produto não encontrado!");
-            return;
+            produtoDAO.delete(id);
+            System.out.println("Produto desativado com sucesso!");
+        } else {
+            System.out.println("Entrada inválida. O ID deve ser um número.");
+            scanner.nextLine();
         }
-
-        // Solicita os novos dados
-        System.out.print("Digite o novo nome do produto: ");
-        String novoNome = scanner.nextLine();
-
-        System.out.print("Digite o novo preço do produto: ");
-        double novoPreco = scanner.nextDouble();
-        scanner.nextLine(); // Consumir a quebra de linha
-
-        // Cria um novo objeto Produto com os novos dados
-        Produto produtoAtualizado = new Produto(novoNome, novoPreco);
-
-        // Chama o método de atualização do DAO
-        produtoDAO.update(id, produtoAtualizado);
-
-        System.out.println("Produto atualizado com sucesso!");
     }
 
+    private void atualizarProdutoService(Scanner scanner) {
+        System.out.print("Digite o ID do produto que deseja atualizar: ");
 
+        if (scanner.hasNextInt()) {
+            int id = scanner.nextInt();
+            scanner.nextLine();
+
+            Produto produtoExistente = produtoDAO.get(id);
+            if (produtoExistente == null) {
+                System.out.println("Produto não encontrado!");
+                return;
+            }
+
+            System.out.print("Digite o novo nome do produto: ");
+            String novoNome = scanner.nextLine();
+
+            System.out.println("Digite sua marca do produto: ");
+            String novoMarca = scanner.nextLine();
+
+            System.out.print("Digite o novo preço do produto: ");
+            double novoPreco = 0;
+
+            try {
+                novoPreco = Double.parseDouble(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Preço inválido! Tente novamente.");
+                return;
+            }
+
+            Produto produtoAtualizado = new Produto(novoNome, novoPreco, novoMarca);
+            produtoDAO.update(id, produtoAtualizado);
+            System.out.println("Produto atualizado com sucesso!");
+        } else {
+            System.out.println("Entrada inválida. O ID deve ser um número.");
+            scanner.nextLine();
+        }
+    }
 }
